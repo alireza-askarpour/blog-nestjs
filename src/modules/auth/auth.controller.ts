571ffd0f4aa0b99543a245971d41d1fa4d521286
Alireza Askarpour <askarpourdev@gmail.com>
2023-06-30 16:56:46 -0400

    feat: implement login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { SignUpDto } from './dtos/signup.dto'
+import { LoginDto } from './dtos/login.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+  async signUp(@Body() signUpDto: SignUpDto) {
     return await this.authService.signup(signUpDto)
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService.login(loginDto)
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,21 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { JwtService } from '@nestjs/jwt'
 import { Model } from 'mongoose'
 import * as bcrypt from 'bcryptjs'
 
 import { User } from '../users/schema/user.schema'
+
 import { SignUpDto } from './dtos/signup.dto'
+import { LoginDto } from './dtos/login.dto'
+
+import { ResponseFormat } from 'src/shared/interfaces/response.interface'
+import { ResponseMessages } from 'src/shared/constants/response-messages.constant'
 
 @Injectable()
 export class AuthService {
@@ -15,26 +25,53 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signup(signupDto: SignUpDto): Promise<{ token: string }> {
+  async signup(signupDto: SignUpDto): Promise<ResponseFormat<any>> {
     try {
       const { fullname, username, password } = signupDto
-
-      const hashedPassword = await bcrypt.hash(password, 10)
-
       const duplicated = await this.userModel.findOne({ username })
       if (duplicated) {
-        throw new BadRequestException('Username already existed')
+        throw new BadRequestException(ResponseMessages.USERNAME_ALREADY_EXISTED)
       }
-
+      const hashedPassword = await bcrypt.hash(password, 10)
       const user = await this.userModel.create({
         fullname,
         username,
         password: hashedPassword,
       })
-
       const token = this.jwtService.sign({ id: user._id })
+      return {
+        statusCode: HttpStatus.CREATED,
+        data: {
+          accessToken: token,
+        },
+      }
+    } catch (error) {
+      throw error
+    }
+  }
 
-      return { token }
+  async login(loginDto: LoginDto): Promise<ResponseFormat<any>> {
+    try {
+      const { username, password } = loginDto
+      const user = await this.userModel.findOne({ username })
+      if (!user) {
+        throw new UnauthorizedException(
+          ResponseMessages.INVALID_USERNAME_PASSWORD,
+        )
+      }
+      const isPasswordMatched = await bcrypt.compare(password, user.password)
+      if (!isPasswordMatched) {
+        throw new UnauthorizedException(
+          ResponseMessages.INVALID_USERNAME_PASSWORD,
+        )
+      }
+      const token = this.jwtService.sign({ id: user._id })
+      return {
+        statusCode: HttpStatus.OK,
+        data: {
+          accessToken: token,
+        },
+      }
     } catch (error) {
       throw error
     }
